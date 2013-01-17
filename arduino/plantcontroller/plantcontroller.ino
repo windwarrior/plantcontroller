7e2bc27e2dcdd202fe90f83e406ac00c9997cf39
Sybe <sybevanhijum_92@hotmail.com>
2013-01-17 12:13:32 +0100

    Voorbeeld webclient toegevoegd

--- arduino/plantcontroller/plantcontroller.ino
+++ arduino/plantcontroller/plantcontroller.ino
@@ -1,20 +1,31 @@
-#include <ethernet.h>
+#include <Ethernet.h>
+#include <SPI.h>
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x00, 0x0D };
 byte ip[]  = {130,89,165,27};
-byte server[] = {130,89,162,163};
+IPAddress server(130,89,162,163);
 //De poort waar de server op luistert
 int port = 8000;
 
-//Client client(server, port);
+EthernetClient client;
 
 void setup(){
     Serial.begin(9600);
+    Ethernet.begin(mac, ip);
+    delay(1000);
+    
+    if(client.connect(server,port)){
+      Serial.println("connected");
+      client.println("GET /plantcontroller HTTP/1.0");
+      client.println();
+    } else {
+      Serial.println("faal :(");
+    }
 
     String msg = "Hallo,";
-    String keys[] = {"key1", "key2", "key3"}
+    String keys[] = {"key1", "key2", "key3"};
     String vals[] = {"hoi", "hai", "blub"};
-    generatePostMessage(&msg, &keys, &vals, 3);
+    generatePostMessage(&msg, keys, vals, 3);
 
     Serial.println(msg);
 }
@@ -23,11 +34,17 @@ void loop(){
 
 }
 
-void generatePostMessage(String * msg, String * keys[], String * vals[], int len){
-  for(int i =0; i<len-1; i++){
-    *msg = *msg + (*keys[i]).replace(" ", "+") + "=" + (*vals[i]).replace(" ", "+") + "&";
+void generatePostMessage(String * msg, String keys[], String vals[], int len){
+  for(int i =0; i<len; i++){
+    String key_repl = keys[i];
+    key_repl.replace(' ', '+');
+    String val_repl = vals[i];
+    val_repl.replace(' ', '+');
+    *msg = *msg + key_repl + "=" + val_repl;
+    if(i < len - 1){
+      *msg = *msg  + "&";
+    }
   }
-  *msg = *msg + (*keys[len-1]).replace(" ", "+") + "=" + (*vals[len-1]).replace(" ", "+");
   Serial.println(*msg);
 }
 
