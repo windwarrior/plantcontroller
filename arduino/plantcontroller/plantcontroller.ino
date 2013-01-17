f0a71a829d590094eb39dec75e593d7a3b2053f9
Sybe <sybevanhijum_92@hotmail.com>
2013-01-17 10:23:37 +0100

    Dingen aangepast zodat het draait op arduino

--- arduino/plantcontroller/plantcontroller.ino
+++ arduino/plantcontroller/plantcontroller.ino
@@ -4,12 +4,12 @@ byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x00, 0x0D };
 byte ip[]  = {130,89,165,27};
 byte server[] = {130,89,162,163};
 //De poort waar de server op luistert
-int port = 8000
+int port = 8000;
 
-Client client(server, port);
+//Client client(server, port);
 
 void setup(){
-    Serial.begin(9600)
+    Serial.begin(9600);
     String msg = "Hallo,";
     generatePostMessage(&msg);
     Serial.println(msg);
@@ -20,9 +20,8 @@ void loop(){
 }
 
 void generatePostMessage(String * msg){
-  String contentMsg = *msg;
-  contentMsg = contentMsg + " hoi";
-  
+  *msg = *msg + "hoi";
+  Serial.println(*msg);
 }
 
 
