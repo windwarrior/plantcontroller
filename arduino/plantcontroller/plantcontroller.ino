e7882df91fe1fb7e0c4c07cec01223789a3d24c4
Sybe <sybevanhijum_92@hotmail.com>
2013-01-24 10:03:16 +0100

    Postmessages in loop gezet, delay erin gezet.

--- arduino/plantcontroller/plantcontroller.ino
+++ arduino/plantcontroller/plantcontroller.ino
@@ -7,6 +7,8 @@ IPAddress server(130,89,162,163);
 //De poort waar de server op luistert
 int port = 8000;
 int humidityPin = A0;
+//De tijd die tussen twee loops zit
+unsigned long time = 5000;
 
 
 EthernetClient client;
@@ -17,43 +19,46 @@ void setup(){
     
     Ethernet.begin(mac);
     delay(1000);
-
-
-    String humidityMsg = "";
-    measureHumidity(&humidityMsg);
-    if(client.connect(server,port)){
-      Serial.println("connected");
-      Serial.println(humidityMsg.length());
-      client.println("POST /plantcontroller/api/add/ HTTP/1.1");
-      client.println("Host: 130.89.162.163");
-      client.println("Connection: close");
-      client.println("Content-Type: application/x-www-form-urlencoded");
-      client.println("Content-Length: " + humidityMsg.length());
-      client.println();
-      client.println(humidityMsg);      
-    } else {
-      Serial.println("faal :(");
-    }
     if (!client.connected()) {
       Serial.println();
       Serial.println("disconnecting.");
       client.stop();
       for(;;)
       ;
+    } else {
+      Serial.println("connected");
     }
 
 }
 
 void loop(){
+  unsigned long beginTime = millis();
+  String humidityMsg = "";
+  measureHumidity(&humidityMsg);
+  if(client.connect(server,port)){
+    Serial.println(humidityMsg.length());
+    client.println("POST /plantcontroller/api/add/ HTTP/1.1");
+    client.println("Host: 130.89.162.163");
+    client.println("Connection: close");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Content-Length: " + humidityMsg.length());
+    client.println();
+    client.println(humidityMsg);
+  }  
   if (client.available()) {
     char c = client.read();
     Serial.print(c);
+  }
+  //Zorgt ervoor dat de loop elke time milliseconden wordt uitgevoerd, moet altijd onderin de loop staan
+  unsigned long endTime = millis();
+  if (endTime - beginTime < time) {
+    delay(endTime - beginTime);
   }
 }
 
 void measureHumidity(String * msg){
   int values = 0;
-  int samples = 10;
+  int samples = 10; // Het aantal samples wat gemeten wordt
   for(int i = 0; i < samples; i++){
     values = values + analogRead(humidityPin);
   }
